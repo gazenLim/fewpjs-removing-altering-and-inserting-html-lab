d5618eded6d0108ae8f0a8b73e96c90d4fe7f1e2
gajen <meetgajen@yahoo.com>
2021-06-24 21:34:37 -0400

    commit tree 326e0c75391886e0040d9fa450df4a2a8a703ad6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+
+
+document.getElementById('main').remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML ="Gajen is the champion"
+document.body.append(newHeader)
